f800b04181d108da0a0d33d5b822ffefa2506205
yuanquanke <3318615021@qq.com>
2022-08-09 11:27:47 +0800

    项目准备-字体图标

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -4,7 +4,10 @@ export default function Login() {
   return (
     <div>Login
       <Button color='primary'> button</Button>
+      <svg className="icon" aria-hidden="true">
+        {/* 使用时，只需要将此处的 iconbtn_like_sel 替换为 icon 的名称即可*/}
+        <use xlinkHref="#iconbtn_like_sel"></use>
+      </svg>
     </div>
-
   )
 }
