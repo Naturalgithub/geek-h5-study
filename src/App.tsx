56400a01d542932385ac1a51248a413ead4b2088
yuanquanke <3318615021@qq.com>
2022-08-09 10:22:16 +0800

    组件库antd-mobile主题定制

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import './App.scss'
 
+// 导入路由
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom'
+
+// 蹈入组件
+import Layout from './pages/Layout'
+import Login from './pages/Login'
+
 function App() {
-  return <div className="app">根组件</div>
+  return (
+    <Router>
+      <div className="app">
+        <Route exact
+          path="/"
+          render={() => <Redirect to="/home"></Redirect>}>
+          <Layout></Layout>
+        </Route>
+        <Route path='/login'>
+          <Login></Login>
+        </Route>
+      </div>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd-mobile'
+
+export default function Login() {
+  return (
+    <div>Login
+      <Button color='primary'> button</Button>
+    </div>
+
+  )
+}
