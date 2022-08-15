a6b81c93b00506b47123d9e5cd80e3e7fd58e28a
yuanquanke <3318615021@qq.com>
2022-08-15 11:26:32 +0800

    如果为空或者手机号格式错误时，让文本框自动获得焦点

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,16 @@
 import { login } from '@/store/actions/login'
 import { LoginForm } from '@/types/data'
 import { Button, Form, Input, List, NavBar, Toast } from 'antd-mobile'
+import { InputRef } from 'antd-mobile/es/components/input'
+import { useRef } from 'react'
 import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router'
 import styles from './index.module.scss'
 export default function Login() {
   const history = useHistory()
   const dispatch = useDispatch()
+  // const formRef = useRef(null)
+  const [form] = Form.useForm()
 
   const onFinish = async (values: LoginForm) => {
     try {
@@ -23,6 +27,26 @@ export default function Login() {
         icon: 'fail'
       })
     }
+  }
+
+  // 给发送验证码绑定点击事件
+  // 在点击事件中获取到文本框的值
+  // 判断文本框的值是否为空
+  // 如果为空或手机号格式错误时，让文本框自动获得焦点
+  // 获取验证码
+  const mobileRef = useRef<InputRef>(null)
+
+  const onGetCode = () => {
+    const mobile = form.getFieldValue('mobile')
+    const errors = form.getFieldError('mobile')
+
+    if (!mobile || errors.length > 0) {
+      // 让手机号自动获取焦点
+      mobileRef.current!.focus()
+      return
+    }
+    console.log('发送请求获取验证！')
+
 
   }
 
@@ -35,7 +59,7 @@ export default function Login() {
         <h2 className="title">账号登录</h2>
 
         {/* 失去焦点的时候以及改变的时候触发校验 */}
-        <Form onFinish={onFinish} validateTrigger={['onChange', 'onBlur']}>
+        <Form onFinish={onFinish} validateTrigger={['onChange', 'onBlur']} form={form}>
           <Form.Item
             className="login-item"
             name="mobile"
@@ -50,11 +74,11 @@ export default function Login() {
               },
             ]}
           >
-            <Input placeholder="请输入用户名" autoComplete='off'></Input>
+            <Input placeholder="请输入用户名" autoComplete='off' ref={mobileRef}></Input>
           </Form.Item>
           <List.Item
             className="login-code-extra"
-            extra={<span className="code-extra">发送验证码</span>}
+            extra={<span className="code-extra" onClick={onGetCode}>发送验证码</span>}
           >
             <Form.Item
               className="login-item"
@@ -74,7 +98,7 @@ export default function Login() {
             </Form.Item>
           </List.Item>
           <Form.Item>
-            <Button color="primary" block className="login-submit" type='submit'>
+            <Button color="primary" block className="login-submit" type='submit' >
               登录
             </Button>
           </Form.Item>
