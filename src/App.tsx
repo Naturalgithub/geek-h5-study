b0cef3308d9de15a82aeb8e89aed6fd2d5633bf6
yuanquanke <3318615021@qq.com>
2022-08-22 14:18:33 +0800

    修改个人信息-页面结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import './App.scss'
+import "./App.scss";
 
 // 导入路由
-import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
 
 // 蹈入组件
-import Layout from './pages/Layout'
-import Login from './pages/Login'
+import Layout from "./pages/Layout";
+import Login from "./pages/Login";
+import ProfileEdit from "./pages/Profile/Edit";
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Route exact
+        <Route
+          exact
           path="/"
-          render={() => <Redirect to="/home"></Redirect>}>
-        </Route>
-        <Route path='/home' component={Layout}>
-        </Route>
-        <Route path='/login' component={Login}>
+          render={() => <Redirect to="/home"></Redirect>}
+        ></Route>
+        <Route path="/home" component={Layout}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/profile/edit">
+          <ProfileEdit />
         </Route>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
