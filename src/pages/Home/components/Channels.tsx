9d8d841a254c776a4b990f842d9084d66ab3467d
yuanquanke <3318615021@qq.com>
2022-08-25 15:49:25 +0800

    频道管理切换频道

--- src/pages/Home/components/Channels.tsx
+++ src/pages/Home/components/Channels.tsx
@@ -5,6 +5,7 @@ import { addChannel, changeActive } from "@/store/actions/home";
 import { Channel } from "@/types/data";
 import { RootState } from "@/types/store";
 import { differenceBy } from "lodash";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./index.module.scss";
 
@@ -38,6 +39,12 @@ const Channels = ({ hide }: Props) => {
     dispatch(addChannel(channel));
   };
 
+  // 是否编辑状态
+  const [isEdit, setIsEdit] = useState(false);
+  const changeEdit = () => {
+    setIsEdit(!isEdit);
+  };
+
   return (
     <div className={styles.root}>
       <div className="channel-header">
@@ -45,11 +52,18 @@ const Channels = ({ hide }: Props) => {
       </div>
       <div className="channel-content">
         {/* 编辑时，添加类名 edit */}
-        <div className={classnames("channel-item")}>
+        <div className={classnames("channel-item", { edit: isEdit })}>
           <div className="channel-item-header">
             <span className="channel-item-title">我的频道</span>
-            <span className="channel-item-title-extra">点击进入频道</span>
-            <span className="channel-item-edit">编辑</span>
+            <span className="channel-item-title-extra">
+              {isEdit ? "点击删除频道" : "点击进入频道"}
+            </span>
+            <span
+              className={classnames("channel-item-edit")}
+              onClick={changeEdit}
+            >
+              {isEdit ? "完成" : "编辑"}
+            </span>
           </div>
           <div className="channel-list">
             {/* 选中时，添加类名 selected */}
@@ -64,7 +78,7 @@ const Channels = ({ hide }: Props) => {
                   key={item.id}
                 >
                   {item.name}
-                  <Icon type="iconbtn_tag_close" />
+                  {isEdit ? <Icon type="iconbtn_tag_close" /> : ""}
                 </span>
               );
             })}
