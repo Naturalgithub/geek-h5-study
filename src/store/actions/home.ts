634810fc2ffcb799450397d4203468ec3763d2aa
yuanquanke <3318615021@qq.com>
2022-08-25 13:14:11 +0800

    获取频道列表数据

--- src/store/actions/home.ts
+++ src/store/actions/home.ts
@@ -1,4 +1,5 @@
 import request from '@/utils/request';
+import { getChannels, hasToken, setChannels } from '@/utils/storage';
 import { ApiResponse, Channel } from './../../types/data.d';
 import { RootThunkAction } from './../../types/store.d';
 
@@ -9,12 +10,44 @@ import { RootThunkAction } from './../../types/store.d';
  */
 export const getUserChannel = (): RootThunkAction => {
   return async dispatch => {
-    const res = await request.get<ApiResponse<{ channels: Channel[] }>>('/user/channels')
-    console.log('actios频道',res.data.data)
-    const {channels} = res.data.data
-    dispatch({
-      type: 'home/saveUserChannels',
-      payload:channels
-    })
+    // const res = await request.get<ApiResponse<{ channels: Channel[] }>>('/user/channels')
+    // console.log('actios频道',res.data.data)
+    // const {channels} = res.data.data
+    // dispatch({
+    //   type: 'home/saveUserChannels',
+    //   payload:channels
+    // })
+
+    // 获取网络数据直接存在redux
+    const getServerData = async () => {
+      const res = await request.get<ApiResponse<{ channels: Channel[] }>>('/user/channels')
+      console.log('actios频道', res.data.data)
+      const { channels } = res.data.data
+      dispatch({
+        type: 'home/saveUserChannels',
+        payload: channels
+      })
+      return res
+    }
+
+    if (hasToken()) {
+      // 登陆了
+      getServerData()
+    } else {
+      // 没有登陆 看本地有没有 有就取本地的频道
+      const channels = getChannels()
+      if (channels.length > 0) {
+        dispatch({
+          type: 'home/saveUserChannels',
+          payload: channels
+        })
+      } else {
+        // 直接获取服务器且保存到本地
+        getServerData()
+        setChannels((await getServerData()).data.data.channels)
+      }
+
+    }
+
   }
- }
+}
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,9 +1,11 @@
-import { Token } from "@/types/data"
+import { Token } from "@/types/data";
+import { Channel } from './../types/data.d';
 
 /**
  * @description: 本地存储封装
  */
 const TOKEN_KEY = 'geek-h5-study-token'
+const CHANNEL_KEY= 'geek-h5-study-channel'
 
 /**
  * @description: 保存token
@@ -37,4 +39,21 @@ export function removeToken() {
  */
 export function hasToken() {
   return !!getToken().token
+}
+
+/**
+ * @description: 保存频道数据
+ * @param {Channel} channels
+ * @return {*}
+ */
+export function setChannels(channels: Channel[]) {
+  localStorage.setItem(CHANNEL_KEY,JSON.stringify(channels))
+}
+
+/**
+ * @description: 获取频道数据
+ * @return {*}
+ */
+export function getChannels() {
+  return JSON.parse(localStorage.getItem(CHANNEL_KEY)|| '[]')
 }
